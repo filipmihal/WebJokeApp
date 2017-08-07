e915eb841b7a53d799a01b76367f47642620e3be
Filip Mihal <fifomihal@gmail.com>
2017-08-07 15:22:05 +0200

    simple joke of the day functionality

---
+++ app/helpers/joke_of_the_day.py
@@ -0,0 +1,13 @@
+"""helper for selecting the joke of the day"""
+import time
+from app.models import Joke
+def current_joke(date):
+    """method select one specific joke which is selected by current date"""
+    valid_jokes_count = Joke.query.count()
+    date_num=time.mktime(time.strptime(date, "%Y-%m-%d"))
+    days_in_date = int(date_num / 86400)
+    current_joke_id = int(days_in_date % valid_jokes_count);
+    if current_joke_id == 0:
+        current_joke_id = valid_jokes_count
+    current_joke = Joke.query.filter_by(id=current_joke_id).first()
+    return current_joke
--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,12 @@
 """base views for Web Joke App"""
 from app import APP
 from flask import render_template
+from app.helpers import joke_of_the_day
+import time
 @APP.route('/')
 def index():
     """Homepage"""
+    today = time.strftime("%Y-%m-%d")
+    current_joke = joke_of_the_day.current_joke(today)
     return render_template('main/index.html',
-                           head_name="Vitajte!")
+                           head_name="Vitajte!", joke_of_the_day=current_joke)
