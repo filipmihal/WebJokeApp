8c61e30dc87b16e76b358d148db6a4ecbf2863d7
Filip Mihal <fifomihal@gmail.com>
2017-08-08 10:11:15 +0200

    very small refractoring of languages.py

--- app/constants/languages.py
+++ app/constants/languages.py
@@ -1,3 +1,4 @@
+"""languages constants"""
 SLOVAK_DAYS_IN_WEEK = ["", "Pondelok", "Utorok", "Streda", "štvrtok", "Piatok", "Sobota", "Nedeľa"]
 SLOVAK_MONTHS = ["", "Január", "Február", "Marec", "Apríl", "Máj", "Jún", "Júl",
-                  "August", "September", "Október", "November", "December"]
+                 "August", "September", "Október", "November", "December"]
